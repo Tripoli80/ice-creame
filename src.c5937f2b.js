1b783f83d1aa6d1bfdc9dff821e04393043e3cbf
Tripoli80 <35936980+Tripoli80@users.noreply.github.com>
2022-05-08 17:04:27 +0000

    Deploying to gh-pages from @ Tripoli80/ice-creame@0fe7322cb1c39145d23f0c57b9ca535077b4bedf 🚀

--- src.c5937f2b.js
+++ src.c5937f2b.js
@@ -4,7 +4,9 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 
 },{}],"RSqK":[function(require,module,exports) {
 !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),c=0;o[c]&&o[c]!==t;)++c;return Boolean(o[c])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype),document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".js-open-modal"),t=document.querySelector(".js-overlay-modal"),o=document.querySelectorAll(".js-modal-close");e.forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var o=this.getAttribute("data-modal");document.querySelector('.modal[data-modal="'+o+'"]').classList.add("active"),t.classList.add("active")})}),o.forEach(function(e){e.addEventListener("click",function(e){this.closest(".modal").classList.remove("active"),t.classList.remove("active")})}),document.body.addEventListener("keyup",function(e){27==e.keyCode&&(document.querySelector(".modal.active").classList.remove("active"),document.querySelector(".overlay").classList.remove("active"))},!1),t.addEventListener("click",function(){document.querySelector(".modal.active").classList.remove("active"),this.classList.remove("active")})});
+},{}],"QutR":[function(require,module,exports) {
+$(document).ready(function(){var a=!0;$(window).on("scroll load resize",function(){if(!a)return!1;var n=$(window).scrollTop(),t=$(".advantages").offset().top,o=$(window).height(),e=$(document).height(),c=$(".advantages").outerHeight();(n+500>=t||o+n==e||c+t<o)&&($(".advantages__calc").css("opacity","1"),$(".advantages__calc-num").spincrement({thousandSeparator:"",duration:1200}),a=!1)})});
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss"),require("./menu"),require("./js/modal");
-},{"./sass/main.scss":"clu1","./menu":"Z6FK","./js/modal":"RSqK"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-creame/src.e58222d6.js.map
+"use strict";require("./sass/main.scss"),require("./menu"),require("./js/modal"),require("./js/calc");
+},{"./sass/main.scss":"clu1","./menu":"Z6FK","./js/modal":"RSqK","./js/calc":"QutR"}]},{},["Focm"], null)
+//# sourceMappingURL=/ice-creame/src.c5937f2b.js.map
