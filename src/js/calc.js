686ff575fc91111043ecc87608da09d727b7826a
Tripoli80 <Tripoli80@gmail.com>
2022-05-08 20:11:57 +0200

    slick add

--- src/js/calc.js
+++ src/js/calc.js
@@ -20,3 +20,26 @@ $(document).ready(function () {
     });
  
 });
+
+$(document).ready(function () { 
+    var show = true;
+    var countbox = ".calc-num";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.calc-num').css('opacity', '1');
+            $('.calc-num').spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+             
+            show = false;
+        }
+    });
+ 
+});
