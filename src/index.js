0fe7322cb1c39145d23f0c57b9ca535077b4bedf
Tripoli80 <Tripoli80@gmail.com>
2022-05-08 19:03:36 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
 import './menu';
-import './js/modal'
+import './js/modal';
+import './js/calc';
 
--- src/js/modal.js
+++ src/js/modal.js
@@ -67,4 +67,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-}); // end ready
+});
