9410f3a661c36163bbd428030c7e589637d33674
Tripoli80 <Tripoli80@gmail.com>
2022-05-11 09:33:35 +0200

    ja modal

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,6 @@
-() => {
+(() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -9,4 +8,4 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
   });
-};
+})();
