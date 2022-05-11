8e46beaf29e90c6af37a7da7d70b590d928754b3
ArtemRe <RechembeiArtem@gmail.com>
2022-05-12 00:05:24 +0300

    finish

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,20 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    menuBtnRef.classList.toggle("is-active");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-    mobileMenuRef.classList.toggle("is-open");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+
+  const menuLinks = document.querySelectorAll('.mobile__links');
+  menuBtnRef.addEventListener('click', () => {
+    handleMenu(menuBtnRef);
+  });
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      handleMenu(menuBtnRef);
+    });
   });
 })();
+function handleMenu(element) {
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const expanded = element.getAttribute('aria-expanded') === 'true' || false;
+  element.classList.toggle('is-active');
+  element.setAttribute('aria-expanded', !expanded);
+  mobileMenuRef.classList.toggle('is-open');
+}
